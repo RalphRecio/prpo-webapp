e3ade7f6093f22560b3ccce991487ffb8abb39f9
Ralph <102205447+RalphRecio@users.noreply.github.com>
2025-07-02 10:08:35 +0800

    Update purchase_requisition.tsx

--- resources/js/pages/prpo/purchase_requisition.tsx
+++ resources/js/pages/prpo/purchase_requisition.tsx
@@ -4,6 +4,7 @@ import { fetchPurchaseRequisition } from '@/hooks/api';
 import { usePaginationService } from '@/hooks/use-pagination-service';
 import AppLayout from '@/layouts/app-layout';
 import { BreadcrumbItem, PurchaseRequisition } from '@/types';
+import { useAuthId } from '@/util/util';
 import { Head, Link } from '@inertiajs/react';
 import axios from 'axios';
 import { Edit, Loader, Plus, Trash } from 'lucide-react';
@@ -125,7 +126,7 @@ export default function PurchaseRequisitionPage() {
                                     href: `/prpo/purchase-request/details/${fullItem?.id}`,
                                 },
 
-                                ...(fullItem && Number(fullItem.is_approve_it_manager) === 0
+                                ...(fullItem && Number(fullItem.is_approve_it_manager) === 0 && Number(fullItem.requestor_id) == Number(useAuthId())
                                     ? [
                                           {
                                               type: 'button',
